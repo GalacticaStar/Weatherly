5088c80077c92d9cc71d07861736cc911ce7d1cc
GalacticaStar <54248527+GalacticaStar@users.noreply.github.com>
2020-10-08 17:53:15 +0100

    Include dist

---
+++ dist/apikeys.js
@@ -0,0 +1,6 @@
+// Positionstack API
+// Forward Geocoding for location search box
+export const accessKey = '5b97b3f2a003f046c4252ea4b17d486a';
+
+// Darksky API Secret Key
+export const secret_key = '3dd6a8dd8ab149dfc213345493c69c12';
